ba8e84d4c93f8f49389e3ef2cad3432bb204e18c
Pavel Naumovich <pavelnaumovic@gmail.com>
2022-11-18 22:03:21 +0000

    apple randomly appears and can be eaten

--- js/script.js
+++ js/script.js
@@ -1,94 +1,85 @@
 const NO_OF_ROWS = 12;
 const NO_OF_COLUMNS = 12;
-let snakePos = [
-  [5, 6],
-  [5, 7],
-  [5, 8],
-];
-let prevSnakePos = [];
-let playFieldMatrix = [];
-
-class CellObj {
-  constructor(isSnake, isApple) {
-    this.isSnake = isSnake;
-    this.isApple = isApple;
-  }
-}
-
 const playArea = document.querySelector("body");
 
 playArea.addEventListener("keydown", arrowPressEventHandler);
 
 function arrowPressEventHandler(e) {
-  runDirection(e.code);
+  let pressedKey = e.code;
+  renderSnake(pressedKey);
 }
 
-//Populate matrix with initial cell objects
-for (let row = 0; row < NO_OF_ROWS; row++) {
-  let matrixRow = [];
-  for (let column = 0; column < NO_OF_COLUMNS; column++) {
-    matrixRow.push(new CellObj(column, row, false, false, false));
-  }
-  playFieldMatrix.push(matrixRow);
-}
-
-const changeSquareColor = (posY, posX, color) => {
+const colorSquare = (posY, posX, color) => {
   let elements = document.getElementById("play-field").children;
   let x = elements.item(posX);
   let xy = x.children[posY];
   xy.style.backgroundColor = color;
 };
 
-//Render Snake on the field
-const renderSnake = () => {
-  runDirection();
+let applePos = [];
+const createApple = () => {
+  if (applePos.length > 0) return;
+  appleX = Math.floor(Math.random() * NO_OF_COLUMNS);
+  appleY = Math.floor(Math.random() * NO_OF_ROWS);
+  applePos = [appleY, appleX];
 };
 
-//Update snake body coords on the field marix.
-const updateSnakePos = () => {
-  //Compare arrays to find what cells will remain in new snake position.
-  for (let i = 0; i < snakePos.length; i++) {
-    for (let k = 0; k < prevSnakePos.length; k++) {
-      if (JSON.stringify(snakePos[i]) === JSON.stringify(prevSnakePos[k])) {
-        prevSnakePos.splice(k, 1);
-      }
-    }
-    let y = snakePos[i][0];
-    let x = snakePos[i][1];
-    playFieldMatrix[y][x].isSnake = true;
-  }
-  //Remove unused cell from previous snake position.
-  prevSnakePos.forEach((cell) => {
-    let y = cell[0];
-    let x = cell[1];
-    playFieldMatrix[y][x].isSnake = false;
-  });
+const renderApple = () => {
+  createApple();
+  colorSquare(applePos[1], applePos[0], "green");
 };
 
-//
-let prevDirection = "ArrowLeft";
-const runDirection = (runDirection) => {
+let snakePos = [
+  [5, 6],
+  [5, 7],
+];
+
+let currentDirection = "ArrowLeft"; //Initial and current snake run direction.
+
+const renderSnake = (runDirection) => {
   let headXpos = snakePos[0][1];
   let headYpos = snakePos[0][0];
 
   if (!runDirection) {
-    runDirection = prevDirection;
+    runDirection = currentDirection;
   }
 
-  if (runDirection === "ArrowUp") headYpos -= 1;
-  if (runDirection === "ArrowDown") headYpos += 1;
-  if (runDirection === "ArrowLeft") headXpos -= 1;
-  if (runDirection === "ArrowRight") headXpos += 1;
+  if (runDirection === "ArrowUp") {
+    headYpos === 0 ? (headYpos = 11) : (headYpos -= 1);
+  } else if (runDirection === "ArrowDown") {
+    headYpos === 11 ? (headYpos = 0) : (headYpos += 1);
+  } else if (runDirection === "ArrowLeft") {
+    headXpos === 0 ? (headXpos = 11) : (headXpos -= 1);
+  } else if (runDirection === "ArrowRight") {
+    headXpos === 11 ? (headXpos = 0) : (headXpos += 1);
+  } else if (runDirection === "stop") {
+    headXpos = snakePos[0][1];
+    headYpos = snakePos[0][0];
+  }
+  currentDirection = runDirection;
 
-  snakePos.unshift([headYpos, headXpos]); //Move first item (head) one cell to the left.
-  let lastSnakeCell = snakePos.at(-1);
+  snakePos.unshift([headYpos, headXpos]); //Move over first square ("head").
+  let lastSquare = snakePos.at(-1); //Find arrays last element. ("tail")
   snakePos.pop(); //Remove last element from array.
-  updateSnakePos();
-  changeSquareColor(lastSnakeCell[1], lastSnakeCell[0], "#DAA520"); //Paint vacated square back to orange.
-  snakePos.forEach((cell) => {
-    changeSquareColor(cell[1], cell[0], "#8888");
+  colorSquare(lastSquare[1], lastSquare[0], "#DAA520"); //Color vacated square back to orange.
+  snakePos.forEach((square) => {
+    colorSquare(square[1], square[0], "#8888");
   });
-  prevDirection = runDirection;
+  checkIfAppleEaten();
 };
 
-setInterval(renderSnake, 1000);
+const checkIfAppleEaten = () => {
+  let snakeHead = snakePos[0];
+  console.log(JSON.stringify(snakeHead) + " " + JSON.stringify(applePos));
+  if (JSON.stringify(snakeHead) === JSON.stringify(applePos)) {
+    applePos = []; //Empty aplle array.
+  }
+};
+
+//Run Snake on the field.
+const run = () => {
+  renderApple();
+  renderSnake();
+};
+
+setInterval(run, 1000 / 10);
